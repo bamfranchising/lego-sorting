9456c394c0dd936c2d3e98666c1ced16001eac4c
James Adams <bmcc1745@gmail.com>
2024-11-18 14:01:57 -0700

    Rewrote to handle servos on both sides and seperate config file for pin numbers

---
+++ ConveyorController/Config.h
@@ -0,0 +1,36 @@
+// File where we can configure the arm angle offsets and pin numbers seperate from the 
+// main block of code.
+#ifndef PIN_AND_ANGLE_CONFIG
+#define PIN_AND_ANGLE_CONFIG
+
+#define RIGHT_ARM_CLOSE_ANGLE 169 // Angle of the closed state of the servos on the right side
+#define RIGHT_ARM_OPEN_ANGLE 104  // Angle of the open state of the servos on the right side (crosses the entire conveyor belt)
+#define LEFT_ARM_CLOSE_ANGLE 10    //Angle of the closed state of the servos on the left side
+#define LEFT_ARM_OPEN_ANGLE 75    //Angle of the open state of the servos on the left side.    
+#define TIME_OPEN 1000          // Time for the arm to remain open after it has arrived
+#define SERVO_TRAVEL_TIME 150   // The time required for the arm to travel between the closed and open states.
+#define BELT_SPEED 17           // Speed of the conveyor belt in cm/s
+#define ARM_LENGTH 28           // Distance between each arm in cm
+#define WINDOW_TRAVEL_TIME ((int)ARM_LENGTH*1000/BELT_SPEED)  // Amount of time it takes for a piece to travel the length of a segment in ms
+#define PRESWING 100            // Arms swing out early by this amount in ms
+
+// There are two different sides the servos are on. We assume the perspective of looking down the conveyor belt
+// in the direction it is moving. Thus, the servos on the right must swing clockwise (as viewed from above) to
+// move across the belt, with the servos on the left moving counterclockwise.
+
+// The servos are numbered as such:
+// 0 | 1
+// 2 | 3
+// 4 | 5
+// 6 | 7
+//  \/
+
+const byte numServos = 8;          // records the number of servos
+const byte servoPinNums[] = {11, 7, 10, 6, 9, 5, 8, 4};  // records the pin numbers of the servos
+const byte openAngles[] = {RIGHT_ARM_OPEN_ANGLE, LEFT_ARM_OPEN_ANGLE};
+const byte closedAngles[] = {RIGHT_ARM_CLOSE_ANGLE, LEFT_ARM_CLOSE_ANGLE};
+// Angle offsets for the closed and open positions. Generally the same for both. For the right side
+// (looking down the conveyor belt) larger numbers means more closed. The opposite is true for the left side
+const int8_t armClosedAngleOffset[] = {5, 0, -5, -5, 0, -5, 0, 0};  // amount the servo is offset in closed position. Larger = more counterclockwise
+const int8_t armOpenAngleOffset[] =  {0, 0, -5, -5, 0, -5, 0, 0};  // amount the servo is offset in open position. Larger = more counterclockwise
+#endif
--- ConveyorController/ConveyorController.ino
+++ ConveyorController/ConveyorController.ino
@@ -1,28 +1,21 @@
 #include <Servo.h>
-#include "SerialReader.h"
 #include <arduino-timer.h>
 
-#define FLIPPER_CLOSE 169 // Angle of the closed state of the servos
-#define FLIPPER_OPEN 104  // Angle of the open state of the servos (crosses the entire conveyor belt)
-#define TIME_OPEN 1500    // Time for the flipper to remain open after it has arrived
-#define TRAVEL_TIME 150   // The time required for the flipper to travel between the closed and open states.
+#include "SerialReader.h"
+#include "Config.h"
 
-const byte numFlippers = 4;          // records the number of servos
-byte servoPinNums[] = {4, 5, 6, 7};  // records the pin numbers of the servos
-int8_t servoCloseOffset[] = {0, 0, -7, 5};  // amount the servo is offset in closed position. Larger = more counterclockwise
-int8_t servoOpenOffset[] =  {0, 0, -7, 0};  // amount the servo is offset in open position. Larger = less open
-Servo flippers[numFlippers];
+Servo servos[numServos];
 
-Timer<20, millis, byte> timer; // Timer to allow for scheduling of tasks in non-blocking manner
+Timer<20, millis, byte> timer; // Timer to allow for scheduling of tasks in non-blocking manner (see arduino-timer library)
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200); // Initialize the serial connection with a baud rate of 115200
 
-  for (byte i = 0; i < numFlippers; i++) {
-    flippers[i].attach(servoPinNums[i]);
-    flippers[i].write(FLIPPER_CLOSE + servoCloseOffset[i]);
-    timer.in(TRAVEL_TIME, killServo, i);
+  // Setup the servos. For each servo
+  for (byte i = 0; i < numServos; i++) {
+    servos[i].attach(servoPinNums[i]);                          // attach to the corresponding pin
+    servos[i].write(closedAngles[i%2] + armClosedAngleOffset[i]); // move the arm to the closed position
+    timer.in(SERVO_TRAVEL_TIME, killServo, i);                  // set task to kill the servo once it arrives in the correct position
   }
 }
 
@@ -34,35 +27,36 @@ void setup() {
 // it always returns false. Returning false may not be strictly necessary
 // as it should always be called by in, which shouldn't repeat, but
 // better safe than sorry.
-bool cycleFlipper(byte flipperNum) {
+bool cycleArm(byte servoNum) {
   // Check that flipper number is valid
-  if (flipperNum < 0 || flipperNum >= numFlippers) return false;
+  if (servoNum < 0 || servoNum >= numServos) return false;
 
-  flippers[flipperNum].attach(servoPinNums[flipperNum]); // attach the servo to the appropriate pin
-  flippers[flipperNum].write(FLIPPER_OPEN+servoOpenOffset[flipperNum]);              // command servo to open angle
-  timer.in(TIME_OPEN+TRAVEL_TIME, closeFlipper, flipperNum);  // schedule the servo to close at the appropriate time
+  servos[servoNum].attach(servoPinNums[servoNum]); // attach the servo to the appropriate pin
+  servos[servoNum].write(openAngles[servoNum%2]+armOpenAngleOffset[servoNum]);  // command servo to open angle
+  timer.in(TIME_OPEN+SERVO_TRAVEL_TIME, closeArm, servoNum);  // schedule the servo to close at the appropriate time
   return false; // don't repeat if called by timer
 }
 
 // Sends signal to close the given flipper and then kills the
-// servo after the TRAVEL_TIME
-bool closeFlipper(byte flipperNum) {
+// servo after the SERVO_TRAVEL_TIME
+bool closeArm(byte servoNum) {
   // Ensure the variable is valid, do nothing otherwise.
-  if (flipperNum < 0 || flipperNum >= numFlippers) return false;
-  // stepFlipper(flipperNum, FLIPPER_OPEN, FLIPPER_CLOSE);
-  flippers[flipperNum].write(FLIPPER_CLOSE + servoCloseOffset[flipperNum]);
-  timer.in(TRAVEL_TIME, killServo, flipperNum);
+  if (servoNum < 0 || servoNum >= numServos) return false;
+  
+  servos[servoNum].write(closedAngles[servoNum%2] + armClosedAngleOffset[servoNum]);
+  timer.in(SERVO_TRAVEL_TIME, killServo, servoNum);
   return false;
 }
 
-// Detaches from the given servo so we sending signals to it.
+// Detaches from the given servo so we stop sending signals to it.
 // The servos selected for this project simply don't engage the
 // motor if no signal is given. Not the case with all servos,
 // but fortunately the case with ours
 bool killServo(byte servoNum) {
   // Ensure the variable is valid, do nothing otherwise.
-  if (servoNum < 0 || servoNum >= numFlippers) return false;
-  flippers[servoNum].detach();
+  if (servoNum < 0 || servoNum >= numServos) return false;
+
+  servos[servoNum].detach();
   return false;
 }
 
@@ -70,7 +64,14 @@ void loop() {
   timer.tick(); // must be called to ensure proper function of timer
   SerialReader.receiveData();  // Looks for data from Serial
   if (SerialReader.hasMessage()) {  // if a whole message has been received from Serial,
-    int flipperNum = atoi(SerialReader.getMessage()); // convert the message to an int (assume it's the number of a servo to move)
-    cycleFlipper(flipperNum); // cycle the given servo number
+    int servoNum = atoi(SerialReader.getMessage()); // convert the message to an int (assume it's the number of a servo to move)
+    
+    short waitTime = 0;//(servoNum/2) * WINDOW_TRAVEL_TIME - PRESWING; // calculate the amount of time the arm has to wait to swing
+    if (waitTime <= 0) { // If the wait time is negative
+      cycleArm(servoNum); // cycle the given servo number right away
+    }
+    else {
+      timer.in(waitTime, cycleArm, servoNum); // Otherwise wait the allotted time and then cycle the arm
+    }
   }
 }
